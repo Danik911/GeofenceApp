66380ef7ec9b778d2f4323d31abbbbad0fe851ed
Daniil <porvipop@mail.ru>
2022-03-04 21:19:50 +0300

    Setup DataStore

--- app/src/main/java/com/example/geofenceapp/util/Constants.kt
+++ app/src/main/java/com/example/geofenceapp/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val PERMISSION_LOCATION_REQUEST_CODE = 1
     const val PERMISSION_BACKGROUND_LOCATION_REQUEST_CODE = 2
+
+    const val PREFERENCE_NAME = "geofence_pref"
+    const val PREFERENCE_FIRST_LAUNCH = "firstLaunch"
 }
